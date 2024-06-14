690dbdf74cca9f586397fda0e2d7375dbf106468
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-06-14 16:15:42 +0200

    fix#5

--- minimal_xfce-alpine.sh
+++ minimal_xfce-alpine.sh
@@ -5,7 +5,7 @@ doas setup-xorg-base xfce4 lightdm-gtk-greeter xfce4-terminal adw-gtk3 adwaita-x
 doas apk add dbus dbus-x11 elogind polkit-elogind
 doas setup-devd udev
 doas rc-update add dbus
-doas apk add pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber
+doas apk add pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber xfce4-pulseaudio-plugin
 doas addgroup $USER audio
 doas apk add gvfs udisks2 ntfs-3g gvfs-cdda gvfs-afp gvfs-goa gvfs-mtp gvfs-smb gvfs-lang gvfs-afc gvfs-nfs gvfs-dev gvfs-archive gvfs-dav gvfs-fuse gvfs-gphoto2 gvfs-avahi
 doas rc-update add lightdm
