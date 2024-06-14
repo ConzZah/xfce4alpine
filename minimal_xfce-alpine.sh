d0e5871efb9c11d14ce09d4718c85bfa91979425
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-06-14 16:46:05 +0200

    fix#7
    added contents from my essentials install script.

--- minimal_xfce-alpine.sh
+++ minimal_xfce-alpine.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-doas apk add wget && wget https://raw.githubusercontent.com/ConzZah/install_essentials4alpine/main/install_essentials4alpine.sh
-doas chmod +x install_essentials4alpine.sh; ./install_essentials4alpine.sh
+doas apk add wget lsblk mount nano ntfs-3g 7zip fastfetch curl git openssh sudo man-pages mandoc bash bash-completion bash-doc python3 w3m w3m-image w3m-doc xz shadow
 doas setup-xorg-base xfce4 lightdm-gtk-greeter xfce4-terminal adw-gtk3 adwaita-xfce-icon-theme adwaita-icon-theme firefox xarchiver mousepad 
 doas apk add dbus dbus-x11 elogind polkit-elogind
 doas setup-devd udev
 doas rc-update add dbus
 doas apk add pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber xfce4-pulseaudio-plugin pavucontrol 
 doas addgroup $USER audio
-doas apk add gvfs udisks2 ntfs-3g gvfs-cdda gvfs-afp gvfs-goa gvfs-mtp gvfs-smb gvfs-lang gvfs-afc gvfs-nfs gvfs-dev gvfs-archive gvfs-dav gvfs-fuse gvfs-gphoto2 gvfs-avahi
+doas apk add gvfs udisks2 gvfs-cdda gvfs-afp gvfs-goa gvfs-mtp gvfs-smb gvfs-lang gvfs-afc gvfs-nfs gvfs-dev gvfs-archive gvfs-dav gvfs-fuse gvfs-gphoto2 gvfs-avahi
 doas rc-update add lightdm
 doas rc-update add elogind
 doas rc-service lightdm start
