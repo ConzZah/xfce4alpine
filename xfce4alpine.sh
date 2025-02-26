41df7332bbd8c8a035c0d1cb19bb1ba38fd3a986
ConzZah <69615452+ConzZah@users.noreply.github.com>
2025-02-26 15:50:37 +0100

    Update xfce4alpine.sh
    cleaned up a bit, added some comments and some important packages

--- xfce4alpine.sh
+++ xfce4alpine.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
-doas apk add alpine-conf wget lsblk mount nano ntfs-3g 7zip fastfetch curl git openssh sudo man-pages mandoc bash bash-completion bash-doc python3 w3m w3m-image w3m-doc xz shadow
-doas apk add flatpak xdg-desktop-portal-gtk; doas apk add dbus dbus-x11 elogind polkit-elogind; doas setup-devd udev; doas rc-update add dbus
-doas setup-xorg-base xfce4 lightdm-gtk-greeter xfce4-terminal xfce4-screensaver xfce4-screenshooter adw-gtk3 adwaita-xfce-icon-theme adwaita-icon-theme firefox xarchiver mousepad 
-doas apk add pipewire pipewire-tools pipewire-pulse pipewire-alsa pipewire-jack wireplumber xfce4-pulseaudio-plugin pavucontrol && doas addgroup $USER audio
+
+# get the basics
+doas apk upgrade -U
+doas apk add alpine-conf wget lsblk mount nano ntfs-3g 7zip fastfetch curl grep sed git openssh sudo man-pages mandoc bash bash-completion bash-doc python3 w3m w3m-image w3m-doc xz shadow
+doas apk add flatpak xdg-desktop-portal-gtk
+
+# setup dbus, udev and xorg:
+doas apk add dbus dbus-x11
+doas rc-update add dbus
+doas setup-devd udev
+doas setup-xorg-base xfce4 lightdm-gtk-greeter xfce4-terminal xfce4-screensaver xfce4-screenshooter xfce-polkit elogind polkit-elogind adw-gtk3 adwaita-xfce-icon-theme adwaita-icon-theme firefox xarchiver mousepad 
+
+# add audio support and install additional packages:
+doas apk add pipewire pipewire-tools pipewire-pulse pipewire-alsa pipewire-jack wireplumber xfce4-pulseaudio-plugin pavucontrol; doas addgroup $USER audio
 doas apk add gnome-disk-utility e2fsprogs-extra gvfs udisks2 gvfs-cdda gvfs-afp gvfs-goa gvfs-mtp gvfs-smb gvfs-lang gvfs-afc gvfs-nfs gvfs-archive gvfs-dav gvfs-fuse gvfs-gphoto2 gvfs-avahi
-doas rc-update add lightdm; doas rc-update add elogind && echo ""; echo ""; echo "[ INSTALL DONE, PLEASE REBOOT!! ]"; echo ""; echo ""; echo "[ PRESS ANY KEY TO EXIT ]"; read -n 1 -s; exit 
+
+# add boot services:
+doas rc-update add elogind
+doas rc-update add lightdm
+doas rc-update add fuse
+
+echo -e "\n[ INSTALL DONE, PLEASE REBOOT!! ]\n\n[ PRESS ANY KEY TO EXIT ]\n"; read -n 1 -s; exit 
