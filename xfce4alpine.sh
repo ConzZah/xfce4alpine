aabdd81412fa514f8eeed729bb5ded0604485dd5
ConzZah <69615452+ConzZah@users.noreply.github.com>
2025-03-08 12:33:29 +0100

    Update xfce4alpine.sh
    - fixed local network browsing

--- xfce4alpine.sh
+++ xfce4alpine.sh
@@ -12,8 +12,13 @@ doas setup-devd udev
 doas setup-xorg-base xfce4 lightdm-gtk-greeter xfce4-terminal xfce4-screensaver xfce4-screenshooter xfce-polkit elogind polkit-elogind adw-gtk3 adwaita-xfce-icon-theme adwaita-icon-theme firefox xarchiver mousepad 
 
 # add audio support and install additional packages:
-doas apk add pipewire pipewire-tools pipewire-pulse pipewire-alsa pipewire-jack wireplumber xfce4-pulseaudio-plugin pavucontrol; doas addgroup $USER audio
+doas apk add pipewire pipewire-tools pipewire-pulse pipewire-alsa pipewire-jack wireplumber xfce4-pulseaudio-plugin pavucontrol; doas addgroup $USER audio; doas addgroup $USER video
 doas apk add gnome-disk-utility e2fsprogs-extra gvfs udisks2 gvfs-cdda gvfs-afp gvfs-goa gvfs-mtp gvfs-smb gvfs-lang gvfs-afc gvfs-nfs gvfs-archive gvfs-dav gvfs-fuse gvfs-gphoto2 gvfs-avahi
+
+# add avahi to allow for local network browsing with thunar:
+doas apk add avahi avahi-tools
+doas rc-update add avahi-daemon
+doas rc-service avahi-daemon start
 
 # add fonts:
 doas apk add fontconfig font-terminus font-inconsolata font-dejavu font-noto font-noto-cjk font-awesome font-noto-extra
